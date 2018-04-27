29f3519a72074200549a109cbe70881aca8cf938
Peter <peterpfleiderer@gmx.de>
2018-04-27 16:39:35 +0200

    working

--- app/views.py
+++ app/views.py
@@ -29,11 +29,7 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('text', usetex=True)
 
-# load RegioClim scripts
-sys.path.append(basepath+'RegioClim')
-import country_analysis; reload(country_analysis)
 import forms
-from plotting import *
 
 import settings
 ind_dict=settings.ind_dict
@@ -74,48 +70,11 @@ def index():
     session['country']   = session["country_avail"][0]
     session['country']   = 'BEN'
 
-    session["ref_period"]   = settings.ref_period
-    session["proj_period"]  = settings.proj_period
-
-    session['use_periods'] = True
-
-    session['warming_lvl_avail']=warming_lvl_dict['en'].keys()
-    session['warming_lvl']='2.0'
-    session['warming_lvl_ref']='ref'
-    index=session['warming_lvl_avail'].index(session['warming_lvl'])
-    session['warming_lvl_avail'][index],session['warming_lvl_avail'][0]=session['warming_lvl_avail'][0],session['warming_lvl_avail'][index]
-
-    session["scenario_avail"]   = settings.scenarios
-    session["scenario"]   = settings.scenarios[0]
-
-    session["dataset_avail"]   = settings.datasets
-    session["dataset"]   = settings.datasets[0]
-
-    session["indicator_avail"]   = ['tas','TXx','pr','RX1','year_RX5']
-    session["indicator"]   = 'tas'
+    session["indicator_avail"]   = ['tas','yield_maize']
+    session["indicator"]   = 'yield_maize'
     index=session['indicator_avail'].index(session['indicator'])
     session['indicator_avail'][index],session['indicator_avail'][0]=session['indicator_avail'][0],session['indicator_avail'][index]
 
-    session['small_region_warning']=False
-
-    session['ref_period_warning']='ok'
-    session['proj_period_warning']='ok'
-
-    session["season_avail"]   = settings.seasons.keys()
-    session["season"]   = 'year'
-
-    session['new_season_name']=''
-    session['new_season_name_auto']=True
-
-    session['id']=str(int((time.time()-int(time.time()))*10000))+str(int(random.random()*100000))
-    session['cou_path']='app/static/COU_sessions/'+session['id']+'_'+session['country']+'.pkl'
-
-
-    session["region_avail"]   = ['asdas','asdas']
-    session['region']   = session["region_avail"][0]
-
-    session['new_region_name']=''
-    session['new_region_name_auto']=True
 
     session['location']='index'
     return redirect(url_for("choices"))
@@ -139,157 +98,26 @@ def choices():
         s['country_avail']=[s['country']]+[sea for sea in s['country_avail'] if sea != s['country']]
         form_country.countrys.choices = zip(s['country_avail'],[settings.country_names[cou][lang] for cou in s['country_avail']])
 
-        # fill the form of region choice - a bit complicated sorting
-        form_region = forms.regionForm(request.form)
-        sorted_regions = ['asdas','asdasas']
-        sorted_regions=[s['region']]+[reg for reg in sorted_regions if (reg != s['region']) & ('+' in reg)]+[reg for reg in sorted_regions if (reg != s['region']) & ('+' not in reg)]
-        form_region.regions.choices = zip(sorted_regions,['sdsad','asdasd'])
-
-        # fill scenario forms
-        form_scenario = forms.scenarioForm(request.form)
-        form_scenario.scenarios.choices = zip(s['scenario_avail'],s['scenario_avail'])
-
-        # form_dataset = forms.datasetForm(request.form)
-        # form_dataset.datasets.choices = zip(s['dataset_avail'],s['dataset_avail'])
-
         # fill indicator forms - restrict for small regions
         form_indicator = forms.indicatorForm(request.form)
-        if s['small_region_warning']:
-            indi_avail_tmp=['tas','pr']
-        else:
-            indi_avail_tmp=s['indicator_avail']
-        form_indicator.indicators.choices = zip(indi_avail_tmp,[indicator_dict[lang][ind][0].upper()+indicator_dict[lang][ind][1:] for ind in indi_avail_tmp])
+        form_indicator.indicators.choices = zip(s['indicator_avail'],[indicator_dict[lang][ind][0].upper()+indicator_dict[lang][ind][1:] for ind in s['indicator_avail']])
 
-        # warming level forms
-        form_warming_lvl = forms.warming_lvlForm(request.form)
-        s['warming_lvl_avail']=[warming_lvl_dict[lang].keys()[warming_lvl_dict[lang].values().index(name)] for name in sorted(warming_lvl_dict[lang].values())]
-        s['warming_lvl_avail']=[s['warming_lvl']]+[sea for sea in s['warming_lvl_avail'] if sea not in [s['warming_lvl'],'ref']]
-        form_warming_lvl.warming_lvls.choices = zip(s['warming_lvl_avail'],[warming_lvl_dict[lang][sea] for sea in s['warming_lvl_avail']])
 
-        form_warming_lvl_ref = forms.warming_lvl_refForm(request.form)
-        s['warming_lvl_avail']=[warming_lvl_dict[lang].keys()[warming_lvl_dict[lang].values().index(name)] for name in sorted(warming_lvl_dict[lang].values())]
-        s['warming_lvl_avail']=[s['warming_lvl_ref']]+[sea for sea in s['warming_lvl_avail'] if sea not in [s['warming_lvl_ref'],'2.0']]
-        form_warming_lvl_ref.warming_lvl_refs.choices = zip(s['warming_lvl_avail'],[warming_lvl_dict[lang][lvl] for lvl in s['warming_lvl_avail']])
 
-        # period forms
-        form_period = forms.PeriodField(request.form)
-        ref_P = str(s["ref_period"][0])+'-'+str(s["ref_period"][1]-1)
-        proj_P = str(s["proj_period"][0])+'-'+str(s["proj_period"][1]-1)
-        form_period = forms.PeriodField(request.form, proj_period=proj_P, ref_period=ref_P)
-
-        # season forms
-        form_season = forms.seasonForm(request.form)
-        s['season_avail']=['year']+ [sea for sea in sorted(season_dict[lang].keys()) if (sea not in ['year','10','11','12']) & ('+' not in sea)]+['10','11','12']+[sea for sea in sorted(season_dict[lang].keys()) if '+' in sea]
-        s['season_avail']=[s['season']]+[sea for sea in s['season_avail'] if sea != s['season']]
-        form_season.seasons.choices = zip(s['season_avail'],[season_dict[lang][sea] for sea in s['season_avail']])
-
-        # prepare periods
-        if s['use_periods']:
-            # periods set by years
-            refP = str(s["ref_period"][0])+'to'+str(s["ref_period"][1]-1)
-            refP_longname=str(s["ref_period"][0])+'-'+str(s["ref_period"][1]-1)
-            refP_clim=refP
-            refP_clim_longname=refP_longname
-            proP=str(s["proj_period"][0])+'to'+str(s["proj_period"][1]-1)
-            proP_longname=str(s["proj_period"][0])+'-'+str(s["proj_period"][1]-1)
-            periods={refP:s["ref_period"],proP:s["proj_period"]}
-            periods_ewembi={refP:s["ref_period"]}
-
-        else:
-            # periods set by warming levels
-            refP = s['warming_lvl_ref']
-            refP_longname=warming_lvl_dict[lang][refP]
-            refP_clim = 'ref'
-            refP_clim_longname=warming_lvl_dict[lang]['ref']
-            proP = s['warming_lvl']
-            proP_longname=warming_lvl_dict[lang][proP]
-            periods=COU._warming_slices
-            periods_ewembi={'ref':[1986,2006]}
-
-        # plot_context is a dictionary combing all information needed by plotting functions in plotting.py
-        indicator_label=indicator_dict[lang][s['indicator']]+' ['+ind_dict[s['indicator']]['unit']+']'
-        plot_context={
-            's':s,
-            'COU':'sdasd',
-            'periods':periods,
-            'periods_ewembi':periods_ewembi,
-            'refP':refP,
-            'refP_clim':refP_clim,
-            'proP':proP,
-            'refP_longname':refP_longname,
-            'refP_clim_longname':refP_clim_longname,
-            'proP_longname':proP_longname,
-            'region':region,
-            'lang':lang,
-            'indicator_label':indicator_label[0].upper()+indicator_label[1:],
-            'season_dict':season_dict,
-            'highlight_region':region,
-            'out_format':'_small.png'
-        }
-
-        # create all plots - see plotting.py
-        EWEMBI_plot='path/to/plot'
-        Projection_plot='path/to/plot'
-        transient_plot='path/to/plot'
-        annual_cycle_plot='path/to/plot'
-        overview_plot='path/to/plot'
-        plt.close('all'); plt.clf()
-        print 'everything plotted '+str(time.time()-start_time)
 
         # the following dicts will fill gaps in choices_en.html with text corresponding to the choices made by the user
         # I'm not sure if this is the most elegant way
-        plot_dict={
-            'EWEMBI_plot':EWEMBI_plot.replace('app/',''),
-            'Projection_plot':Projection_plot.replace('app/',''),
-            'transient_plot':transient_plot.replace('app/',''),
-            'annual_cycle_plot':annual_cycle_plot.replace('app/',''),
-            'overview_plot':overview_plot.replace('app/',''),
-        }
+        context={
+            'firr_map':'static/plots_maps/'+s['country']+'_firr_2p0.png',
+            'noirr_map':'static/plots_maps/'+s['country']+'_noirr_2p0.png',
+            'boxplot':'static/plots_boxplot/'+'plot_delta_yield_actual_co2_'+settings.country_names[s['country']]['en']+'.png',
 
-        if s['season']=='year':season_add_on=''
-        if s['season']!='year' and lang=='en':season_add_on=' in '+season_dict[lang][s['season']]
-        if s['season']!='year' and lang=='fr':season_add_on=' en '+season_dict[lang][s['season']]
-
-        if s['use_periods']==False:
-            refP_clim_longname=refP_clim_longname.replace('°C','°C '+settings.above_preindustrial[lang])
-            refP_longname=refP_longname.replace('°C','°C '+settings.above_preindustrial[lang])
-            proP_longname=proP_longname.replace('°C','°C '+settings.above_preindustrial[lang])
-
-        plot_text_dict={
-            'indicator':indicator_dict[lang][s['indicator']],
-            'season_add_on':season_add_on,
-            'refP_longname':refP_longname,
-            'proP_longname':proP_longname,
-            'refP_clim_longname':refP_clim_longname,
-        }
-
-        other_dict={
-            'language':get_language_tag(),
-            'use_periods':s['use_periods'],
-            'small_region_warning':s['small_region_warning'],
-            'ref_period_warning':s['ref_period_warning'],
-            'proj_period_warning':s['proj_period_warning'],
-            'language_flag':languages[s['language']],
-        }
-
-        form_dict = {
             'form_country':form_country,
-            'form_region':form_region,
-            'form_period':form_period,
-            'form_warming_lvl':form_warming_lvl,
-            'form_warming_lvl_ref':form_warming_lvl_ref,
-            'form_season':form_season,
-            'form_scenario':form_scenario,
             'form_indicator':form_indicator,
+
+            'indicator':indicator_dict[lang][s['indicator']],
         }
 
-        # all dicts are combined into one dict which is than passed to the template
-        context=form_dict.copy()
-        context.update(other_dict)
-        context.update(plot_dict)
-        context.update(plot_text_dict)
-        context.update(text_dict[lang])
-        context.update(button_dict[lang])
 
         session['location']='choices'
         return render_template('choices_'+lang+'.html',**context)
@@ -307,7 +135,7 @@ def choices():
 ###############################
 # option choices
 ###############################
-ute('/indicator_choice',  methods=('POST', ))
+@app.route('/indicator_choice',  methods=('POST', ))
 def indicator_choice():
   form_indicator = forms.indicatorForm(request.form)
   session['indicator']=form_indicator.indicators.data
@@ -321,7 +149,12 @@ def indicator_choice():
   if session["season"] not in session["season_avail"]: session["season"] = 'year'
   return redirect(url_for('choices'))
 
+@app.route('/country_choice',  methods=('POST', ))
+def country_choice():
+  form_country = forms.countryForm(request.form)
+  session['country']=form_country.countrys.data
 
+  return redirect(url_for('choices'))
 ###############################
 # Download
 ###############################
